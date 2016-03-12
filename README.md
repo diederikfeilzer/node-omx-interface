4ab6651cdb523f3b682ac2ae52b7fe76be96e376
Diederik Feilzer <diederikfeilzer@MacBook-Air-van-Diederik.local>
2016-03-12 23:21:15 +0100

    Clean up, volume control

--- README.md
+++ README.md
@@ -5,14 +5,26 @@ An interface between nodejs and the omxplayer via dbus.
 This project is in beta. Many functionalities work but I'm working on many more!
 
 # Syntax
+```
 var omx = require('omx-interface');
 
-omx.open('test.h624',{audioOutput:'hdmi',blackBackground:true,disableKeys:true,disableOnScreenDisplay:true})
+omx.open('test.h624',{audioOutput:'hdmi', blackBackground:true, disableKeys:true, disableOnScreenDisplay:true})
 
+omx.setPosition(60*5); //set position to 5 minutes in to the movie
+```
 # remote
-The "omx-interface" package comes with an optional remote for your mobile phone. In the remote app you can browse files and control the player over your local network. Unlike other omx middleware get methods are supported. So the current time and duration are available!
+The "omx-interface" package comes with an optional remote for your mobile phone. In the remote app you can browse files and control the player over your local network. Unlike other OMX middleware, GET and SET methods are supported rather than just emulating keypresses. So the current time, duration and volume are available!
 
 <img src="https://raw.githubusercontent.com/diederikfeilzer/node-omx-interface/master/Screenshot.png" alt="Screenshot" width=300>
+
+Since the remote is more of a showcase of the capabilities of the middleware it is optional and has to be initiated before use.
+
+```
+var omx = require('omx-interface');
+omx.init_remote({port:8000});
+```
+
+The remote can be located from any browser on the local network. The webpage can be added to the home screen for a more app like experiance.
 
 # options
 ## general options
@@ -34,49 +46,54 @@ subtitlePath:            default: ""
 
 # properties
 ## Get duration of current track/movie in seconds
-omx.getCurrentDuration();
+``omx.getCurrentDuration();``
 
 ## Get position of current track/movie in seconds
-omx.getCurrentPosition();
+`omx.getCurrentPosition();``
 
-This function can be called many times per second without bothering the dbus since the position is extrapolated from the short term cached paying status. 
+This function can be called many times per second without bothering the DBus since the position is extrapolated from the short term cached paying status.
 
-## Get volume in mili db
-omx.getCurrentVolume(); //NOT YET IMPLEMENTED
+## Get volume as fraction of max (0.0 - 1.0)
+``omx.getCurrentVolume();``
 
 # methods
 
-## jump to point in file/seek relative to current position
-omx.seek(seconds); //UNTESTED
+## jump to point in file/seek relative to current position (-Inf to +Inf)
+``omx.seek(seconds);``
 
 ## jump to point in file/seek relative to start point (absolute)
-omx.setPosition(seconds); //UNTESTED
+``omx.setPosition(seconds);``
 
 ## stop playing
-omx.stop();
+``omx.stop();``
 
 ## quit omxplayer
-omx.quit();
+``omx.quit();``
 
 ## pause omxplayer
-omx.pause();
+``omx.pause();``
+
 Note: Unlike hitting the spacebar, this method pauses only when playing and remains paused when allready paused.
 
 ## resume playing
-omx.play();
+``omx.play();``
+
 Note: Unlike hitting the spacebar, this method starts playing only when paused and remains playing when allready playing.
 
 ## toggle pause/play
-omx.togglePlay();
+``omx.togglePlay();``
+
 Note: Same as spacebar function of omxplayer.
 
 ## volume up
-omx.volumeUp();
+``omx.volumeUp();``
+
 Note: Same as "+" key in omxplayer.
 
 ## volume down
-omx.volumeDown();
+``omx.volumeDown();``
+
 Note: Same as "-" key in omxplayer.
 
-## set volume to a disired decibel level.
-omx.setVolume(vol); //NOT YET IMPEMENTED
+## set volume to a fraction of the max volume (0.0 - 1.0)
+``omx.setVolume(vol);``
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 exec('mkfifo omxpipe');
 
-var cache = {
+var defaults = {
 	path:{
 		value:'',
 		time:new Date(),
@@ -32,7 +32,7 @@ var cache = {
 	}
 };
 
-var defaults = cache;
+var cache = defaults;
 
 dbus = "bash "+__dirname+"/dbus.sh ";
 
@@ -53,7 +53,7 @@ var play = function() {
 				cache.isPlaying.valid = true;
 			}
 		}
-    });
+  });
 }
 
 var pauseTryCount = 0;
@@ -73,7 +73,7 @@ var pause = function() {
  				cache.isPlaying.valid = true;
 			}
 		}
-    });
+  });
 }
 
 var stopTryCount = 0;
@@ -88,7 +88,7 @@ var stop = function() {
 			stopTryCount = 0;
 			cache = defaults;
 		}
-    });
+  });
 }
 
 var quitTryCount = 0;
@@ -103,7 +103,7 @@ var quit = function() {
 			quitTryCount = 0;
 			cache = defaults;
 		}
-    });
+  });
 }
 
 var togglePlayTryCount = 0;
@@ -115,7 +115,7 @@ var togglePlay = function() {
 		} else {
 			togglePlayTryCount = 0;
 		}
-    });
+  });
 }
 
 var volumeUpTryCount = 0;
@@ -127,7 +127,7 @@ var volumeUp = function() {
 		} else {
 			volumeUpTryCount = 0;
 		}
-    });
+  });
 }
 
 var volumeDownTryCount = 0;
@@ -139,13 +139,11 @@ var volumeDown = function() {
 		} else {
 			volumeDownTryCount = 0;
 		}
-    });
+  });
 }
 
-//setVolumeTo
-
 var seekTryCount = 0;
-var seek = function(offset) {
+var seek = function(offset) { //seek offset in seconds; relative from current position; negative values will cause a jump back;
 	exec(dbus + 'seek '+Math.round(offset*1000000),function(error, stdout, stderr) {
 		if(error && seekTryCount < 4){
 			seekTryCount++;
@@ -154,11 +152,11 @@ var seek = function(offset) {
 			seekTryCount = 0;
 			update_position();
 		}
-    });
+  });
 }
 
 var setPositionTryCount = 0;
-var setPosition = function(position) {
+var setPosition = function(position) { //position in seconds from start; //positions larger than the duration will stop the player;
 	exec(dbus + 'setposition '+Math.round(position*1000000),function(error, stdout, stderr) {
 		if(error && setPositionTryCount < 4){
 			setPositionTryCount++;
@@ -167,7 +165,20 @@ var setPosition = function(position) {
 			setPositionTryCount = 0;
 			update_position();
 		}
-    });
+  });
+}
+
+var setVolumeTryCount = 0;
+var setVolume = function(volume) { //volume should be set from 0.0 to 1.0; Above 1.0 is depreciated;
+	exec(dbus + 'setvolume '+volume,function(error, stdout, stderr) {
+		if(error && setPositionTryCount < 4){
+			setVolumeTryCount++;
+			setVolume(volume);
+		} else {
+			setVolumeTryCount = 0;
+			update_volume();
+		}
+  });
 }
 
 var toggleSubtitlesTryCount = 0;
@@ -179,7 +190,7 @@ var toggleSubtitles = function() {
 		} else {
 			toggleSubtitlesTryCount = 0;
 		}
-    });
+  });
 }
 
 var hideSubtitlesTryCount = 0;
@@ -191,7 +202,7 @@ var hideSubtitles = function() {
 		} else {
 			hideSubtitlesTryCount = 0;
 		}
-    });
+  });
 }
 
 var showSubtitlesTryCount = 0;
@@ -203,52 +214,46 @@ var showSubtitles = function() {
 		} else {
 			showSubtitlesTryCount = 0;
 		}
-    });
+  });
 }
-	
+
 var update_position = function() {
-	exec(dbus + 'position',function(error, stdout, stderr) {
+	exec(dbus + 'getposition',function(error, stdout, stderr) {
 		if (error) return false;
 		var position = parseInt(stdout);
 		cache.position.value = position;
 		cache.position.time = new Date();
 		cache.position.valid = true;
-    });
+  });
 }
 
 var update_status = function() {
-	exec(dbus + 'playstatus',function(error, stdout, stderr) {
+	exec(dbus + 'getplaystatus',function(error, stdout, stderr) {
 		if (error) return false;
-		if (stdout.indexOf("Playing")>-1) {
- 			cache.isPlaying.value = 1;
- 			cache.isPlaying.time = new Date();
-			cache.isPlaying.valid = true;
-		} else {
-			cache.isPlaying.value = 0;
- 			cache.isPlaying.time = new Date();
-			cache.isPlaying.valid = true;
-		}
-    });
+		cache.isPlaying.value = ((stdout.indexOf("Playing")>-1) ? 1 : 0);
+ 		cache.isPlaying.time = new Date();
+		cache.isPlaying.valid = true;
+  });
 }
 
 var update_duration = function() {
-	exec(dbus + 'duration',function(error, stdout, stderr) {
+	exec(dbus + 'getduration',function(error, stdout, stderr) {
 		if (error) return false;
-        var duration = Math.round(Math.max(0,Math.round(parseInt(stdout.substring((stdout.indexOf("int64")>-1 ? stdout.indexOf("int64")+6:0)))/10000)/100));
+    var duration = Math.round(Math.max(0,Math.round(parseInt(stdout.substring((stdout.indexOf("int64")>-1 ? stdout.indexOf("int64")+6:0)))/10000)/100));
 		cache.duration.value = duration;
 		cache.duration.time = new Date();
 		cache.duration.valid = true;
-    });
+  });
 }
 
 var update_volume = function() {
-	exec(dbus + 'volume',function(error, stdout, stderr) {
+	exec(dbus + 'getvolume',function(error, stdout, stderr) {
 		if (error) return false;
-        var volume = parseFloat(stdout);
+    var volume = parseFloat(stdout);
 		cache.volume.value = volume;
 		cache.volume.time = new Date();
 		cache.volume.valid = true;
-    });
+  });
 }
 
 var getCurrentPosition = function(){
@@ -276,42 +281,54 @@ var getCurrentStatus = function(){
 }
 
 var getCurrentDuration = function(){
-	if(!cache.duration.valid || (cache.duration.value < 1)) {
+	if(cache.duration.value <= 0) {
+		cache.duration.valid = false;
+	}
+	if(!cache.duration.valid) {
 		update_duration();
 	}
 	return cache.duration.value;
 }
 
 var getCurrentVolume = function(){
-	if(!cache.volume.valid || (cache.volume.value < 1)) {
+	if(!cache.volume.valid) {
 		update_volume();
 	}
 	return cache.volume.value;
 }
 
 var onProgress = function(callback){
-	setInterval(function(){if(cache.isPlaying.value){callback({position:getCurrentPosition(),duration:getCurrentDuration()});}},1000);
+	setInterval(function(){
+		if(getCurrentStatus()){
+			callback({position:getCurrentPosition(), duration:getCurrentDuration(), volume:getCurrentVolume()});
+		}
+	},1000);
 }
+
+/*
+end_called = false;
+var onEnd = function(callback){
+	setInterval(function(){
+		if (cache.duration.valid && cache.position.value > cache.duration.value) {
+			callback();
+			end_called = true;
+		}
+	},1000);
+}
+*/
 
 var open = function (path, options) {
 	var settings = options || {};
 	var args = [];
 	var command = 'omxplayer';
-	
+
+	cache = defaults;
+
 	cache.path.value = path;
 	cache.path.valid = true;
-	
-	cache.position.value = 0;
-	cache.position.valid = false;
-	
-	cache.duration.value = 0;
-	cache.duration.valid = false;
-	
-	cache.isPlaying.value = 0;
-	cache.isPlaying.valid = false;
-	
+
 	args.push('"'+path+'"');
-    
+
 	if (['hdmi','local','both'].indexOf(settings.audioOutput) != -1) {
 		args.push('-o');
 		args.push(settings.audioOutput);
@@ -330,182 +347,173 @@ var open = function (path, options) {
 	}
 
 	if (settings.disableGhostbox === true) {
-		args.push('--no-ghost-box'); 
+		args.push('--no-ghost-box');
 	}
 
 	if (settings.subtitlePath && settings.subtitlePath != "" ){
-		args.push('--subtitles'); 
-		args.push('"'+settings.subtitlePath+'"'); 
+		args.push('--subtitles');
+		args.push('"'+settings.subtitlePath+'"');
 	}
-	
+
 	args.push('--dbus_name');
 	args.push('org.mpris.MediaPlayer2.omxplayer');
 
-	var cmd = command+' '+args.join(' ')+' < omxpipe';
-	
-    exec(cmd,function(error, stdout, stderr) {
-        console.log(stdout);
-    });
-    exec(' . > omxpipe');
-    
-    update_duration();
+  exec(command+' '+args.join(' ')+' < omxpipe',function(error, stdout, stderr) {
+  	console.log(stdout);
+  });
+  exec(' . > omxpipe');
+
+  update_duration();
 
 };
 
 var init_remote = function(options){
 
 	var settings = options || {};
-	
+
 	settings.port = (settings.port || 8000);
-	
+
 	var fs = require('fs');
 	var path = require('path');
 	var app = require('express')();
 	var server = require('http').Server(app);
 	var io = require('socket.io')(server);
-	
+
 	server.listen(settings.port);
 
 	app.get('/',function(req, res){
 		res.sendFile(__dirname+'/remote.html');
 	});
 
-	app.get('/files',function(req, res){
-
-		/*
-				Addapted from: https://chawlasumit.wordpress.com/2014/08/04/how-to-create-a-web-based-file-browser-using-nodejs-express-and-jquery-datatables/
-		*/
- 		
- 		var query = req.query.query || process.env.HOME || process.env.USERPROFILE;
- 		
+	app.get('/files',function(req, res){ // Thanks to https://chawlasumit.wordpress.com/2014/08/04/how-to-create-a-web-based-file-browser-using-nodejs-express-and-jquery-datatables/
+		var query = req.query.query || process.env.HOME || process.env.USERPROFILE;
 		fs.readdir(query, function (err, files) {
-     		if (err) {
-        		throw err;
-      		}
-      		var data = [];
-      		data.push({ Name : '../', IsDirectory: true, Path : path.join(query,'../')  });
-      		files.forEach(function (file) {
-        		try {
-           	  		var isDirectory = fs.statSync(path.join(query,file)).isDirectory();
+			if (err) {
+				throw err;
+			}
+			var data = [];
+			data.push({ Name : '../', IsDirectory: true, Path : path.join(query,'../')  });
+			files.forEach(function (file) {
+				try {
+					var isDirectory = fs.statSync(path.join(query,file)).isDirectory();
 					if (isDirectory) {
 						if(file.substr(0, 1) != ".") {
-	        	 	   		data.push({ Name : file, IsDirectory: true, Path : path.join(query, file)  });
-	        	 	   	}
-      	  	  		} else {
-   	    	 			var ext = path.extname(file);
-	 					if(file.substr(0, 1) != ".") {
+							data.push({ Name : file, IsDirectory: true, Path : path.join(query, file)  });
+						}
+					} else {
+						var ext = path.extname(file);
+						if(file.substr(0, 1) != ".") {
 							data.push({ Name : file, Ext : ext, IsDirectory: false, Path : path.join(query, file) });
-    	        		} 
-       	        	}
+						}
+					}
 				} catch(e) {
-      				console.log(e); 
-       			}       
-       		});
+					console.log(e);
+				}
+			});
 			data = _.sortBy(data, function(f) { return f.Name });
 			res.json(data);
 		});
-			
-		/*
-			end source
-		*/
 	});
 
 	io.on('connection', function (socket){
-	
-		setInterval(function(){
+
+		setInterval(function(){ //TODO: push updates after events rather than each second.
 			var data = {};
 			data.duration=getCurrentDuration();
 			data.position=getCurrentPosition();
 			data.status=getCurrentStatus();
+			data.volume=getCurrentVolume();
 			data.path=cache.path.value;
 			data.name=path.basename(cache.path.value);
-			data.volume=null; //temp.
-			data.subtitles=null; //temp.
+			data.subtitles=null; //for future .srt file array.
 			data.time = new Date();
 			socket.emit('notification', data); //io.volatile.emit vs io.emit;
 		},1000);
-			
+
 		socket.on('open', function (data) {
-			open(data.path,{blackBackground:true,audioOutput:'both',disableKeys:true,disableOnScreenDisplay:true});
+			open(data.path,{
+				blackBackground:true,
+				audioOutput:'both',
+				disableKeys:true,
+				disableOnScreenDisplay:true
+			});
 		});
-		
+
 		socket.on('togglePlay', function (data) {
 			togglePlay();
 		});
-		
+
 		socket.on('play', function (data) {
 			play();
 		});
-		
+
 		socket.on('pause', function (data) {
 			pause();
 		});
-		
+
 		socket.on('stop', function (data) {
 			stop();
 		});
-		
+
 		socket.on('quit', function (data) {
 			quit();
 			process.exit(1);
 		});
-		
+
 		socket.on('poweroff', function (data) {
 			quit();
 			exec('sudo shutdown -h -P now');
 		});
-		
+
 		socket.on('volumeUp', function (data) {
 			volumeUp();
 		});
-		
+
 		socket.on('volumeDown', function (data) {
 			volumeDown();
 		});
-		
+
 		socket.on('setPosition', function (data) {
 			setPosition(data.position);
 		});
-		
+
 		socket.on('seek', function (data) {
 			seek(data.offset);
 		});
-		
+
 		socket.on('seekFastBackward', function (data) {
 			seek(-300);
 		});
-		
+
 		socket.on('seekFastForward', function (data) {
 			seek(300);
 		});
-		
+
 		socket.on('seekBackward', function (data) {
 			seek(-30);
 		});
-		
+
 		socket.on('seekForward', function (data) {
 			seek(30);
 		});
-		
-		/*
+
 		socket.on('setVolume', function (data) {
 			setVolume(data.volume);
 		});
-		*/
-		
+
 		socket.on('toggleSubtitles', function (data) {
 			toggleSubtitles();
 		});
-		
+
 		socket.on('showSubtitles', function (data) {
 			showSubtitles();
 		});
-		
+
 		socket.on('hideSubtitles', function (data) {
 			hideSubtitles();
 		});
 	});
-	
+
 
 	var os = require('os');
 	var address = require('network-address');
@@ -516,15 +524,13 @@ var init_remote = function(options){
 	var ip='?';
 	for (var dev in ifaces)
 		{
-			ifaces[dev].forEach(function(details)
-				{
-					if (details.family=='IPv4' && details.address!='127.0.0.1')
-						{
-							ip=details.address;
-						}
-			  	});
+			ifaces[dev].forEach(function(details) {
+				if (details.family=='IPv4' && details.address!='127.0.0.1') {
+					ip=details.address;
+				}
+			});
 		}
-	
+
 	console.log('remote at: '+ip+':'+settings.port);
 	return true;
 }
@@ -540,10 +546,11 @@ module.exports.volumeUp = volumeUp;
 module.exports.volumeDown = volumeDown;
 module.exports.seek = seek;
 module.exports.setPosition = setPosition;
+module.exports.setVolume = setVolume;
 module.exports.toggleSubtitles = toggleSubtitles;
 module.exports.hideSubtitles = hideSubtitles;
 module.exports.showSubtitles = showSubtitles;
 module.exports.getCurrentPosition = getCurrentPosition;
 module.exports.getCurrentDuration = getCurrentDuration;
+module.exports.getCurrentVolume = getCurrentVolume;
 module.exports.onProgress = onProgress;
-	
